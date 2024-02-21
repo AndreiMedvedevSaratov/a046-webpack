e5603ae5d690eeb0ed45656295bcd9675fc9732c
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-02-21 09:30:26 +0400

    Add typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = (env) => {
   return {
     mode: env.mode ?? "development",
-    entry: path.resolve(__dirname, "src", "index.js"),
+    entry: path.resolve(__dirname, "src", "index.ts"),
     output: {
       path: path.resolve(__dirname, "build"),
       filename: "[name].[contenthash].js",
@@ -14,6 +15,19 @@ module.exports = (env) => {
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "public", "index.html"),
       }),
+      new webpack.ProgressPlugin(),
     ],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
   };
 };
