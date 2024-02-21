249b2f88d550e8a3d84597002ba11d7bd20a6c35
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-02-21 11:19:02 +0400

    Update webpack dev server

--- webpack.config.ts
+++ webpack.config.ts
@@ -11,6 +11,8 @@ interface EnvVariables {
 }
 
 export default (env: EnvVariables) => {
+  const isDev = env.mode === 'development';
+
   const config: webpack.Configuration = {
     mode: env.mode ?? "development",
     entry: path.resolve(__dirname, "src", "index.ts"),
@@ -37,11 +39,11 @@ export default (env: EnvVariables) => {
     resolve: {
       extensions: [".tsx", ".ts", ".js"],
     },
-    devtool: 'inline-source-map',
-    devServer: {
+    devtool: isDev ? 'inline-source-map' : false,
+    devServer: isDev ? {
       port: env.port ?? 5000,
       open: true,
-    }
+    } : undefined
   };
 
   return config;
